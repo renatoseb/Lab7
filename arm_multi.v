62d40541d8499bf1b7e08032fe1d9bd0da97f409
fbarrueta22 <frings-22@hotmail.com>
2020-11-25 19:35:54 -0500

    completed multicycle

---
+++ arm_multi.v
@@ -0,0 +1,316 @@
+module arm (
+	clk,
+	reset,
+	MemWrite,
+	Adr,
+	WriteData,
+	ReadData
+);
+	input wire clk;
+	input wire reset;
+	output wire MemWrite;
+	output wire [31:0] Adr;
+	output wire [31:0] WriteData;
+	input wire [31:0] ReadData;
+	wire [31:0] Instr;
+	wire [3:0] ALUFlags;
+	wire PCWrite;
+	wire RegWrite;
+	wire IRWrite;
+	wire AdrSrc;
+	wire [1:0] RegSrc;
+	wire [1:0] ALUSrcA;
+	wire [1:0] ALUSrcB;
+	wire [1:0] ImmSrc;
+	wire [1:0] ALUControl;
+	wire [1:0] ResultSrc;
+	controller c(
+		.clk(clk),
+		.reset(reset),
+		.Instr(Instr[31:12]),
+		.ALUFlags(ALUFlags),
+		.PCWrite(PCWrite),
+		.MemWrite(MemWrite),
+		.RegWrite(RegWrite),
+		.IRWrite(IRWrite),
+		.AdrSrc(AdrSrc),
+		.RegSrc(RegSrc),
+		.ALUSrcA(ALUSrcA),
+		.ALUSrcB(ALUSrcB),
+		.ResultSrc(ResultSrc),
+		.ImmSrc(ImmSrc),
+		.ALUControl(ALUControl)
+	);
+	datapath dp(
+		.clk(clk),
+		.reset(reset),
+		.Adr(Adr),
+		.WriteData(WriteData),
+		.ReadData(ReadData),
+		.Instr(Instr),
+		.ALUFlags(ALUFlags),
+		.PCWrite(PCWrite),
+		.RegWrite(RegWrite),
+		.IRWrite(IRWrite),
+		.AdrSrc(AdrSrc),
+		.RegSrc(RegSrc),
+		.ALUSrcA(ALUSrcA),
+		.ALUSrcB(ALUSrcB),
+		.ResultSrc(ResultSrc),
+		.ImmSrc(ImmSrc),
+		.ALUControl(ALUControl)
+	);
+endmodule
+
+
+
+module decode (
+	clk,
+	reset,
+	Op,
+	Funct,
+	Rd,
+	FlagW,
+	PCS,
+	NextPC,
+	RegW,
+	MemW,
+	IRWrite,
+	AdrSrc,
+	ResultSrc,
+	ALUSrcA,
+	ALUSrcB,
+	ImmSrc,
+	RegSrc,
+	ALUControl
+);
+	input wire clk;
+	input wire reset;
+	input wire [1:0] Op;
+	input wire [5:0] Funct;
+	input wire [3:0] Rd;
+	output reg [1:0] FlagW;
+	output wire PCS;
+	output wire NextPC;
+	output wire RegW;
+	output wire MemW;
+	output wire IRWrite;
+	output wire AdrSrc;
+	output wire [1:0] ResultSrc;
+	output wire [1:0] ALUSrcA;
+	output wire [1:0] ALUSrcB;
+	output wire [1:0] ImmSrc;
+	output wire [1:0] RegSrc;
+	output reg [1:0] ALUControl;
+	wire Branch;
+	wire ALUOp;
+
+	// Main FSM
+	mainfsm fsm(
+		.clk(clk),
+		.reset(reset),
+		.Op(Op),
+		.Funct(Funct),
+		.IRWrite(IRWrite),
+		.AdrSrc(AdrSrc),
+		.ALUSrcA(ALUSrcA),
+		.ALUSrcB(ALUSrcB),
+		.ResultSrc(ResultSrc),
+		.NextPC(NextPC),
+		.RegW(RegW),
+		.MemW(MemW),
+		.Branch(Branch),
+		.ALUOp(ALUOp)
+	);
+
+	// ADD CODE BELOW
+	// Add code for the ALU Decoder and PC Logic.
+	// Remember, you may reuse code from previous labs.
+	// ALU Decoder
+    
+    //DOING
+    always @(*)
+        if (ALUOp) 
+        begin // which Data-processing Instr?
+            case(Funct[4:1])
+                4'b0100: ALUControl = 2'b00; // ADD
+                4'b0010: ALUControl = 2'b01; // SUB
+                4'b0000: ALUControl = 2'b10; // AND
+                4'b1100: ALUControl = 2'b11; // ORR
+                default: ALUControl = 2'bx; // unimplemented
+            endcase
+            FlagW[1] = Funct[0];    // update N & Z flags if S bit is set
+            FlagW[0] = Funct[0] & (ALUControl == 2'b00 | ALUControl == 2'b01);
+        end 
+        else 
+        begin
+            ALUControl = 2'b00; // add for non data-processing instructions
+            FlagW = 2'b00;      // don't update Flags
+        end
+
+	// PC Logic
+    //DOING
+    assign PCS = ((Rd == 4'b1111) & RegW) | Branch; 
+
+	// Instr Decoder
+    // Add code for the Instruction Decoder (Instr Decoder) below.
+	// Recall that the input to Instr Decoder is Op, and the outputs are
+	// ImmSrc and RegSrc. We've completed the ImmSrc logic for you.
+	
+    assign ImmSrc = Op;
+
+    //DOING
+    assign RegSrc[0] = (Op == 2'b10); // read PC on Branch
+    assign RegSrc[1] = (Op == 2'b01); // read Rd on STR 
+
+endmodule
+
+// ADD CODE BELOW
+// Add code for the condlogic and condcheck modules. Remember, you may
+// reuse code from prior labs.
+module condlogic (
+	clk,
+	reset,
+	Cond,
+	ALUFlags,
+	FlagW,
+	PCS,
+	NextPC,
+	RegW,
+	MemW,
+	PCWrite,
+	RegWrite,
+	MemWrite
+);
+	input wire clk;
+	input wire reset;
+	input wire [3:0] Cond;
+	input wire [3:0] ALUFlags;
+	input wire [1:0] FlagW;
+	input wire PCS;
+	input wire NextPC;
+	input wire RegW;
+	input wire MemW;
+	output wire PCWrite;
+	output wire RegWrite;
+	output wire MemWrite;
+
+	wire [1:0] FlagWrite;
+	wire [3:0] Flags;
+	wire CondEx;
+
+	// Delay writing flags until ALUWB state
+	flopr #(2) flagwritereg(
+		clk,
+		reset,
+		FlagW & {2 {CondEx}},
+		FlagWrite
+	);
+
+	// ADD CODE HERE
+    //Reutilizado del Lab06
+    //DOING 
+    flopenr #(2) flagreg1(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[1]),
+		.d(ALUFlags[3:2]),
+		.q(Flags[3:2])
+	);
+	flopenr #(2) flagreg0(
+		.clk(clk),
+		.reset(reset),
+		.en(FlagWrite[0]),
+		.d(ALUFlags[1:0]),
+		.q(Flags[1:0])
+	);
+	condcheck cc(
+		.Cond(Cond),
+		.Flags(Flags),
+		.CondEx(CondEx)
+	);
+	assign FlagWrite = FlagW & {2 {CondEx}};
+	assign RegWrite = RegW & CondEx;
+	assign MemWrite = MemW & CondEx;
+	assign PCSrc = PCS & CondEx;
+
+endmodule
+
+module condcheck (
+	Cond,
+	Flags,
+	CondEx
+);
+	input wire [3:0] Cond;
+	input wire [3:0] Flags;
+	output reg CondEx;
+
+	// ADD CODE HERE
+    //Reutilizado del LAB06
+
+    wire neg;
+	wire zero;
+	wire carry;
+	wire overflow;
+	wire ge;
+	assign {neg, zero, carry, overflow} = Flags;
+	assign ge = neg == overflow;
+	always @(*)
+		case (Cond)
+			4'b0000: CondEx = zero;
+			4'b0001: CondEx = ~zero;
+			4'b0010: CondEx = carry;
+			4'b0011: CondEx = ~carry;
+			4'b0100: CondEx = neg;
+			4'b0101: CondEx = ~neg;
+			4'b0110: CondEx = overflow;
+			4'b0111: CondEx = ~overflow;
+			4'b1000: CondEx = carry & ~zero;
+			4'b1001: CondEx = ~(carry & ~zero);
+			4'b1010: CondEx = ge;
+			4'b1011: CondEx = ~ge;
+			4'b1100: CondEx = ~zero & ge;
+			4'b1101: CondEx = ~(~zero & ge);
+			4'b1110: CondEx = 1'b1;
+			default: CondEx = 1'bx;
+		endcase
+endmodule
+
+//Reutilizado de L06
+module flopenr (
+	clk,
+	reset,
+	en,
+	d,
+	q
+);
+	parameter WIDTH = 8;
+	input wire clk;
+	input wire reset;
+	input wire en;
+	input wire [WIDTH - 1:0] d;
+	output reg [WIDTH - 1:0] q;
+	always @(posedge clk or posedge reset)
+		if (reset)
+			q <= 0;
+		else if (en)
+			q <= d;
+endmodule
+
+module flopr (
+	clk,
+	reset,
+	d,
+	q
+);
+	parameter WIDTH = 8;
+	input wire clk;
+	input wire reset;
+	input wire [WIDTH - 1:0] d;
+	output reg [WIDTH - 1:0] q;
+	always @(posedge clk or posedge reset)
+		if (reset)
+			q <= 0;
+		else
+			q <= d;
+endmodule
